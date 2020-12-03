ebae8645aef436809c7a0c65c976d708d500dcba
takehito <fukuda_kento@fukehito.com>
2020-12-03 22:28:24 +0900

    メッセージの追加

--- main_test.go
+++ main_test.go
@@ -1,11 +1,15 @@
 package main
 
-import "testing"
+import (
+	"fmt"
+	"testing"
+)
 
 func TestMain1(t *testing.T) {
 	t.Fatal("_________________________error_____________________________________")
 }
 func TestMain2(t *testing.T) {
+	fmt.Println("::is ignore from github log:: _________________________error_____________________________________")
 	t.Fatal("::is ignore from github log:: _________________________error_____________________________________")
 }
 func TestMain3(t *testing.T) {
